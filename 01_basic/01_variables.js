282e910f65e9027ea69370770d737c974b0cb4b3
deekshithn645 <deekshithn06@gmail.com>
2024-08-21 13:09:16 +0000

    variables concept

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 12345
+let   accountEmail = 'deekshithn06@gmail.com'
+var   accountPassword = '12345'
+accountState = 'banglore'
+console.table([accountId,accountEmail,accountPassword,accountState])
