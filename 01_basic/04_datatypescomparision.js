c173c6f366b7e7b022bd5270d9a432a6573ac4b9
deekshithn645 <deekshithn06@gmail.com>
2024-08-24 18:19:11 +0000

    datatype_comparision

---
+++ 01_basic/04_datatypescomparision.js
@@ -0,0 +1,24 @@
+let a=10
+let b=10
+console.log(a<b);
+console.log(1==1);
+console.log('2'>2);
+console.log(true==true);
+console.log(String<String);
+console.log(String==String);
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+
+
+
+
+
+
+
+
+
+
