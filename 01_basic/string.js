6ff0d07fe91e78dc2c8436e30d420daa54ced192
deekshithn645 <deekshithn06@gmail.com>
2024-08-25 05:48:44 +0000

    update

---
+++ 01_basic/string.js
@@ -0,0 +1,8 @@
+const name = new String('deekshith');
+// console.log(name[0]);
+// console.log(name.length);
+// console.log(name.at(0,4));
+// console.log(name.toUppercase());
+
+// let newString = 
+console.log(name.toUpperCase());
